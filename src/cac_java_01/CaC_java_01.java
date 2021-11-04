84292c79c0e02ec733d2abbd395c3cc22f3e7bba
victor <vgrusz@yahoo.com.ar>
2021-11-04 16:58:40 -0300

    versión 0.9 beta

--- src/cac_java_01/CaC_java_01.java
+++ src/cac_java_01/CaC_java_01.java
@@ -4,6 +4,8 @@
  */
 package cac_java_01;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author victor
@@ -14,8 +16,32 @@ public class CaC_java_01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         
+        
+        String nombre = JOptionPane.showInputDialog("Ingrese su nombre");
+        String apellido = JOptionPane.showInputDialog("Ingrese su apellido");
+        String edad = JOptionPane.showInputDialog("Ingrese su edad");
+        String hobbie = JOptionPane.showInputDialog("Ingrese su hobbie");
+        String editor = JOptionPane.showInputDialog("Ingrese su editor de código preferido,");
+        String so = JOptionPane.showInputDialog("Ingrese el sistema operativo que utiliza,");
+        
+        System.out.println(nombre);
+        System.out.println(apellido);
+        System.out.println(edad);
+        System.out.println(hobbie);
+        System.out.println(editor);
+        System.out.println(so);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
         
         
     }
